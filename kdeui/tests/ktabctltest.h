ce4a32fe52ef09d8f5ff1dd22c001110902b60a2
toma <toma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-11-25 17:56:58 +0000

    Copy the KDE 3.5 branch to branches/trinity for new KDE 3.5 features. BUG:215923
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/trinity/kdelibs@1054174 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ kdeui/tests/ktabctltest.h
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ */
+
+#ifndef _KTABCTLTEST_H
+#define _KTABCTLTEST_H
+
+#include <qwidget.h>
+class KTabCtl;
+class QPushButton;
+class QMultiLineEdit;
+
+class TopLevel : public QWidget
+{
+    Q_OBJECT
+public:
+
+    TopLevel( QWidget *parent=0, const char *name=0 );
+protected:
+    void resizeEvent(QResizeEvent *);
+    KTabCtl *test;
+    QPushButton *ok, *cancel;
+    QMultiLineEdit *e;
+    QWidget *pages[3];
+public slots:
+    void okPressed();
+    void tabChanged(int);
+};
+
+#endif
