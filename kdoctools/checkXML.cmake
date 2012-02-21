cce6e81f38f0d6eb0a45fe5e5bf1d5a44b184e2a
Darrell Anderson <humanreadable@yahoo.com>
2012-02-21 17:39:21 -0600

    Fix the creation of the checkXML script. This partially resolves bug report 694.

--- kdoctools/checkXML.cmake
+++ kdoctools/checkXML.cmake
@@ -3,5 +3,5 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 # FIXME this must be separate?
 exec_prefix=@CMAKE_INSTALL_PREFIX@
-exit `@CMAKE_INSTALL_PREFIX@/@BIN_INSTALL_DIR@/meinproc --check --stdout $@ > /dev/null`
+exit `@BIN_INSTALL_DIR@/meinproc --check --stdout $@ > /dev/null`
 
