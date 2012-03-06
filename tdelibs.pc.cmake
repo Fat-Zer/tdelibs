592b5277b540ff566a3808f86e558d8e76d00741
Timothy Pearson <kb9vqf@pearsoncomputing.net>
2012-03-06 11:37:00 -0600

    Add TDELibs .pc This closes Bug 882 Thanks to Aleksey Midenkov for the patch!

---
+++ tdelibs.pc.cmake
@@ -0,0 +1,7 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=@EXEC_INSTALL_PREFIX@
+libdir=@LIB_INSTALL_DIR@
+includedir=@INCLUDE_INSTALL_DIR@
+
+Name: TDELibs
+Description: Core libraries for Trinity
