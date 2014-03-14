dd7861dbd765b6eb08c791913920aced2f2ddd83
Michele Calgaro <michele.calgaro@yahoo.it>
2014-03-14 14:29:57 +0900

    Updated "About TDE" dialog text. This relates to bug 2001.

--- tdeui/tdeabouttde.cpp
+++ tdeui/tdeabouttde.cpp
@@ -32,14 +32,19 @@ TDEAboutKDE::TDEAboutKDE( TQWidget *parent, const char *name, bool modal )
 		 parent, name, modal )
 {
   const TQString text1 = i18n(""
-    "The <b>Trinity Desktop Environment</b> is a fork of the "
+    "The <b>Trinity Desktop Environment</b> was born as a fork of the "
     "K Desktop Environment version 3.5, which was originally written by the KDE Team, "
     "a world-wide network of software engineers committed to <a "
     "href=\"http://www.gnu.org/philosophy/free-sw.html\">Free Software</a> "
-    "development.<br><br>No single group, company or organization controls the "
+    "development. The name <i>Trinity</i> was chosen because the word means "
+    "<i>Three</i> as in <i>continuation of KDE 3</i>.<br>Since then, TDE has evolved to be "
+    "an indipendent and standalone computer desktop environment project. The developers "
+    "have molded the code to its own identity without giving up on the efficiency, "
+    "productivity and traditional user interface experience characteristic of the "
+    "original KDE 3 series.<br><br>No single group, company or organization controls the "
     "Trinity source code. Everyone is welcome to contribute to Trinity.<br><br>Visit <A "
-    "HREF=\"http://www.trinitydesktop.org/\">http://www.trinitydesktop.org</A> for more information "
-    "about Trinity, and <A HREF=\"http://www.kde.org/\">http://www.kde.org</A> "
+    "HREF=\"http://www.trinitydesktop.org\">http://www.trinitydesktop.org</A> for more information "
+    "about Trinity, and <A HREF=\"http://www.kde.org\">http://www.kde.org</A> "
     "for more information on the KDE project. ");
 
   const TQString text2 = i18n(""
@@ -47,7 +52,7 @@ TDEAboutKDE::TDEAboutKDE( TQWidget *parent, const char *name, bool modal )
     "do so. However, you - the user - must tell us when "
     "something does not work as expected or could be done better.<br><br>"
     "The Trinity Desktop Environment has a bug tracking system. Visit "
-    "<A HREF=\"http://bugs.pearsoncomputing.net/\">http://bugs.pearsoncomputing.net</A> or "
+    "<A HREF=\"http://bugs.pearsoncomputing.net\">http://bugs.pearsoncomputing.net</A> or "
     "use the \"Report Bug...\" dialog from the \"Help\" menu to report bugs.<br><br>"
     "If you have a suggestion for improvement then you are welcome to use "
     "the bug tracking system to register your wish. Make sure you use the "
@@ -59,22 +64,24 @@ TDEAboutKDE::TDEAboutKDE( TQWidget *parent, const char *name, bool modal )
     "program interfaces. You can provide graphics, themes, sounds, and "
     "improved documentation. You decide!"
     "<br><br>"
-    "Visit "
-    "<A HREF=\"http://www.trinitydesktop.org/jobs/\">http://www.trinitydesktop.org/jobs/</A> "
-    "for information on some projects in which you can participate."
+    "Visit the "
+    "<A HREF=\"http://www.trinitydesktop.org/jobs\">TDE Development</A> "
+    "webpage to find out how you can contribute or mail us using one of the "
+    "available <A HREF=\"http://www.trinitydesktop.org/mailinglist.php\">mailing lists</A>."
     "<br><br>"
     "If you need more information or documentation, then a visit to "
-    "<A HREF=\"http://www.trinitydesktop.org/docs/\">http://www.trinitydesktop.org/docs/</A> "
+    "<A HREF=\"http://www.trinitydesktop.org/docs\">http://www.trinitydesktop.org/docs</A> "
     "will provide you with what you need.");
+
   const TQString text4 = i18n(""
-    "Trinity is available free of charge, but making it is not free.<br><br>"
-    "<br><br>"
-    "The Trinity team does need financial support. Most of the money is used to "
-    "reimburse members and others on expenses they experienced when "
-    "contributing to Trinity. You are encouraged to support Trinity through a financial "
-    "donation, using one of the ways described at "
+    "TDE is available free of charge, but making it is not free.<br><br>"
+    "The Trinity team <i>does need</i> financial support. The money is used to "
+    "support the expenses incurred to keep the TDE servers running, so that you - "
+    "the user - can access them at any time. You are encouraged to support Trinity "
+    "through a financial or hardware donation, using one of the ways described at "
     "<a href=\"http://www.trinitydesktop.org/donate.php\">http://www.trinitydesktop.org/donate.php</a>."
     "<br><br>Thank you very much in advance for your support!");
+
   setHelp( TQString::fromLatin1("khelpcenter/main.html"), TQString::null );
   setTitle(i18n("Trinity Desktop Environment. Release %1").
 	   arg(TQString::fromLatin1(TDE_VERSION_STRING)) );
