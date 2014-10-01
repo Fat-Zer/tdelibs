Fat-Zer/tdelibs

commit 427ebd3a978e7b2e844b7a821163d138c77eb4d3

Rename kde.pot to tde.pot This relates to Bug 1910

tdecore/kdebug.h
@@ -25,6 +25,8 @@
 #include <tqstring.h>
 #include "tdelibs_export.h"
 
+#undef NDEBUG
+
 class TQWidget;
 class TQDateTime;
 class TQDate;

CMakeLists.txt
@@ -1002,7 +1002,7 @@ set( CMAKE_MODULE_LINKER_FLAGS "-Wl,--no-undefined" )
 ##### headers ###################################
 
 install( FILES
-    kde.pot
+    tde.pot
   DESTINATION ${INCLUDE_INSTALL_DIR} )
 
 

Makefile.am.in
@@ -36,7 +36,7 @@ $(top_srcdir)/acinclude.m4: $(top_srcdir)/libltdl/ltdl.m4
 AUTOMAKE_OPTIONS = foreign 1.6.1
 
 potdir = $(kde_includes)
-pot_DATA = kde.pot
+pot_DATA = tde.pot
 
 messages:
 	cd tdeabc/scripts && perl ./makeaddressee
@@ -55,12 +55,12 @@ messages:
 	find $$dirs -maxdepth 1 -name "*.h" -print >> files ;\
 	echo ./tdecore/tde-config.cpp.in >> files ;\
 	$(EXTRACTRC) `find $$dirs -maxdepth 1 \( -name "*.rc" -o -name "*.ui" \) ` ./dnssd/kcm_tdednssd.kcfg > rc.cpp; \
-	: > kde.pot ;\
+	: > tde.pot ;\
 	$(XGETTEXT) -ktranslate -x qt-messages.pot rc.cpp `cat files` && cat messages.po qt-messages.pot > $(podir)/tdelibs.pot
-	$(XGETTEXT) common_texts.cpp -o kde.pot
+	$(XGETTEXT) common_texts.cpp -o tde.pot
 	rm -f dirs
 
-EXTRA_DIST = admin kde.pot \
+EXTRA_DIST = admin tde.pot \
 	COMPILING COMPILING.html COPYING.BSD DEBUG \
 	KDE2PORTING.html NAMING common_texts.cpp tdelibs.lsm kdoc.rules \
 	qt-messages.pot configure.in.in configure.in.mid configure.in.bot

tdecore/ksock.cpp
@@ -330,15 +330,20 @@ bool TDEServerSocket::init( unsigned short int _port )
   return true;
 }
 
+// RAJA FIXME DEBUG ONLY
+extern char *__progname;
+
 bool TDEServerSocket::bindAndListen()
 {
   if (d == NULL || d->ks == NULL)
     return false;
 
+printf("[RAJA DEBUG 640.0] In TDEServerSocket::bindAndListen [%s]\n\r", __progname); fflush(stdout);
 
   int ret = d->ks->listen( SOMAXCONN );
   if (ret < 0)
     {
+printf("[RAJA DEBUG 640.1] Error listening on socket [%d]\n\r", ret); fflush(stdout);
         kdWarning(170) << "Error listening on socket: " << ret << "\n";
 	delete d->ks;
 	d->ks = NULL;

tdeioslave/iso/Makefile.am
@@ -62,6 +62,6 @@ uninstall-local:
 messages:
 	LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`; \
 	if test -n "$$LIST"; then \
-		 $(XGETTEXT) -C -ki18n -x $(kde_includes)/kde.pot $$LIST -o ../po/iso.pot; \
+		 $(XGETTEXT) -C -ki18n -x $(kde_includes)/tde.pot $$LIST -o ../po/iso.pot; \
 	fi
 

commit 2d5d779de976726a1ae3d310a2a04ff0ce2cebac

Fixed tdebase FTBFS caused by commit 427ebd3 when compiling with NDEBUG. Commit 427ebd3 remove the definition of NDEBUG if kdebug.h is included in the compilation process. If such file is incluced in a .cpp file and not in the corresponding .h file, FTBFS happens due to different class interface between the class declaration and class definition.

tdecore/kdebug.h
@@ -25,8 +25,6 @@
 #include <tqstring.h>
 #include "tdelibs_export.h"
 
-#undef NDEBUG
-
 class TQWidget;
 class TQDateTime;
 class TQDate;